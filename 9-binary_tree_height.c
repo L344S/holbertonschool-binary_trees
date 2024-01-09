88e7372e3277e7b783122f3badc37e6ad3b8b67e
L344S <7474@holbertonstudents.com>
2024-01-09 06:34:39 +0100

    Implemented binary_tree_height function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,40 +1,46 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree.
- * @tree: A pointer to the root node of the tree.
- *
- * Return: The height of the tree, or 0 if the tree is NULL.
- *
- * Description: This function calculates the height of a binary tree
- *              by recursively traversing its left and right subtrees.
- *              The height is the length of the longest path from
- *              the root to a leaf node. If the tree is NULL, the height is 0.
- */
+* binary_tree_height - entry point
+* @tree: pointer to the root node of the binary tree
+*
+* --------------- Description ---------------
+* This function measures the height of a binary tree.
+* A binary tree is a tree who have at most 2 children
+* (left child and right child).
+*
+* Return: 0 if tree is NULL or height of the binary tree
+*/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t height_left_subtree; /* Store the height of the left subtree */
+	size_t height_right_subtree; /* Store the height of the right subtree */
+
+	/* If tree is NULL, return 0 */
 	if (tree == NULL)
-		return (0);
+		return (0); /* Base case, for recursion (stop condition) */
 
-	size_t height = 0;
-	const binary_tree_t *current = tree;
+    /* Calculate the height of the left subtree */
+	if (tree->left != NULL) /* If the left subtree is not NULL */
+		/* Add 1 to the height of the left subtree */
+		/* and call the function recursively */
+		height_left_subtree = 1 + binary_tree_height(tree->left);
+	else
+		/* If the left subtree does not exist, initialize it to 0 */
+		height_left_subtree = 0;
 
-	while (current->left || current->right)
-	{
-		size_t left_height = 0, right_height = 0;
+    /* Calculate the height of the right subtree */
+	if (tree->right != NULL) /* If the right subtree is not NULL */
+		/* Add 1 to the height of the right subtree */
+		/* and call the function recursively */
+		height_right_subtree = 1 + binary_tree_height(tree->right);
+	else
+		/* If the right subtree does not exist, initialize it to 0 */
+		height_right_subtree = 0;
 
-		if (current->left)
-			left_height = 1 + binary_tree_height(current->left);
-
-		if (current->right)
-			right_height = 1 + binary_tree_height(current->right);
-
-		if (left_height > right_height)
-			height = left_height;
-		else
-			height = right_height;
-
-		break;
-	}
-	return (height);
+    /* Return the height of the longest subtree */
+	if (height_left_subtree > height_right_subtree)
+		return (height_left_subtree);
+	else
+		return (height_right_subtree);
 }
